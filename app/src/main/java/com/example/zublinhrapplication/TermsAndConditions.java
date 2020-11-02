73371752cbb5dda10b55f6b14fccfa79d9c3c33d
Christian Childress <cschildress@crimson.ua.edu>
2020-11-02 17:10:55 -0600

    Made Changes to add terms and conditions when registering a user

---
+++ app/src/main/java/com/example/zublinhrapplication/TermsAndConditions.java
@@ -0,0 +1,14 @@
+package com.example.zublinhrapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsAndConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms_and_conditions_view);
+    }
+}
